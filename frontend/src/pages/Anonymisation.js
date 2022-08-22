8ea7af08f8872174c63099c07dce1d1aecd832cb
whippet83 <bergeron.nicolas83@gmail.com>
2022-08-22 11:44:32 -0400

    Navbar working

--- frontend/src/pages/Anonymisation.js
+++ frontend/src/pages/Anonymisation.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "../App.css";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer";
 
 function Anonymisation() {
     return (
         <>
 
             <div>
+                <Navbar />
                 Anonymisation
-
+                <Footer/>
             </div>
         </>
     );
